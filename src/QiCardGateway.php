ff140767576a36f6493bf6f216648f63bb9afc08
00bx <zamanmerani49@gmail.com>
2025-08-21 04:27:49 +0300

    config folder.

--- src/QiCardGateway.php
+++ src/QiCardGateway.php
@@ -1,13 +1,52 @@
 <?php
 
 namespace Thebrightlabs\IraqPayments;
+use Illuminate\Support\Facades\Http;
 
 class QiCardGateway
 {
  // Bismillah.
-    public function paymentDone()
+
+    protected $config;
+
+    public function __construct()
     {
-        return true;
+        $this->config = config('qi_card');
+    }
+
+    public function getUsername()
+    {
+        $mode = $this->config['mode'];
+        return $this->config[$mode]['username'];
+    }
+
+    public function getPassword()
+    {
+        $mode = $this->config['mode'];
+        return $this->config[$mode]['password'];
+    }
+
+    public function getTerminalId()
+    {
+        $mode = $this->config['mode'];
+        return $this->config[$mode]['terminal_id'];
+    }
+
+    public function getApiHost()
+    {
+        $mode = $this->config['mode'];
+        return $this->config[$mode]['api_host'];
+    }
+
+    public function checkStatus()
+    {
+        // very basic status check
+        $username = $this->getUsername();
+        $password = $this->getPassword();
+        $apiHost = $this->getApiHost();
+        return Http::withBasicAuth($username,$password)
+            ->get($apiHost."/status");
+
     }
 
 }
